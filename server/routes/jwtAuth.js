f35d41612290d83aaef3ba7a079b1134a595fe79
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-05-10 20:31:14 +0700

    add jwt token

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-
 const pool = require('../db');
+const bcrypt = require('bcrypt');
+
+const jwtGenerator = require('../utils/jwtGenerator');
 
 // register
 router.post('/register', async (req, res) => {
@@ -15,6 +17,23 @@ router.post('/register', async (req, res) => {
     if (user.rows.length !== 0) {
       res.status(401).send('user already exist');
     }
+
+    // bcrypt password
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+    // menambah user baru ke database
+    const newUser = await pool.query(
+      'INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *',
+      [name, email, bcryptPassword]
+    );
+
+    // membuat token jwt
+    const token = jwtGenerator(newUser.rows[0].user_id);
+
+    res.json({ token });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error yes');
