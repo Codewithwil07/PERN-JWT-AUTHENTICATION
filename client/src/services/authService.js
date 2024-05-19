55bef1e4d1cf6b0136b83d2ea39c40924ca35887
wilywangceo@gmail.com <wilywangceo@gmail.com>
2024-05-19 11:04:47 +0700

    add fiture login & logout

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -10,6 +10,26 @@ const register = (name, email, password) => {
   });
 };
 
+const login = (email, password) => {
+  return axios
+    .post(API_URL + 'login', {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.token) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+      }
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem('user');
+};
+
 export default {
   register,
+  login,
+  logout,
 };
